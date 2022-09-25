6648179e45e5fbdc4e76dc0090d65bba2a9e31d0
HVH2k6 <hahhung09092006@gmail.com>
2022-09-25 08:06:16 +0700

    a

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const infoOld = document.querySelector('.info');
 const btn = document.querySelector('.btn');
 btn.onclick = function () {
   var numberOld = parseInt(input.value);
+
   if (!numberOld) {
     infoOld.textContent = 'tuổi nhập vào không hợp lệ';
+  } else if (numberOld <= 0) {
+    infoOld.textContent = 'mày chưa sinh sao tao tính được tuổi';
+  } else if (numberOld >= 100) {
+    infoOld.textContent = 'con lạy cụ cụ sống dai quá';
   } else {
     infoOld.textContent = 'tuổi của bạn là: ' + numberOld;
   }
